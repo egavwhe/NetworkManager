e7690858c7f7dfaf59e8f54d462fd2eb2d2b2d74
Gavin <gavin.john.whelan@gmail.com>
2016-09-16 00:49:16 +0100

    Adding in routing config.

--- networkManagerApp/app.js
+++ networkManagerApp/app.js
@@ -1,4 +1,18 @@
-var app = angular.module("networkApp", [])
+var app = angular.module("networkManager", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "templates/home.html",
+                controller: "homeController"
+            })
+            .when("/network", {
+                templateUrl: "templates/network.html",
+                controller: "networkController"
+            })
+    })
+    .controller("homeController", function ($scope) {
+        $scope.message = "Welcome!";
+    })
     .controller("networkController", function ($scope) {
-        $scope.message = "Network Config Manager";
+        $scope.networkName = "Network 1";
     })
