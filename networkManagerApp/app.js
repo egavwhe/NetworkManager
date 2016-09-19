99154cb3201cc05ec930e912a3b07d1dbe324680
Gavin <gavin.john.whelan@gmail.com>
2016-09-19 23:55:25 +0100

    Added filters to table.

--- networkManagerApp/app.js
+++ networkManagerApp/app.js
@@ -17,9 +17,11 @@ var app = angular.module("networkManager", ["ngRoute"])
         var nodes = [
             {
                 id: 1,
-                type: "RBS",
+                type: "rbs",
                 location: "Galway",
                 traffic: "heavy",
+                repairCost: "12000",
+                installDate: new Date("January 17. 2015"),
                 cells: [
                     {
                         range: "medium"
@@ -28,9 +30,50 @@ var app = angular.module("networkManager", ["ngRoute"])
             },
             {
                 id: 2,
-                type: "LTE",
+                type: "Lte",
                 location: "Dublin",
                 traffic: "light",
+                repairCost: "23000",
+                installDate: new Date("November 17. 2012"),
+                cells: [
+                    {
+                        range: "small"
+                    }
+                ]
+            },
+            {
+                id: 3,
+                type: "Lte",
+                location: "Cork",
+                traffic: "medium",
+                repairCost: "5000",
+                installDate: new Date("March 3. 2010"),
+                cells: [
+                    {
+                        range: "small"
+                    }
+                ]
+            },
+            {
+                id: 4,
+                type: "eNodeB",
+                location: "Belfast",
+                traffic: "light",
+                repairCost: "40000",
+                installDate: new Date("April 10. 2016"),
+                cells: [
+                    {
+                        range: "small"
+                    }
+                ]
+            },
+            {
+                id: 5,
+                type: "nodeB",
+                location: "Donegal",
+                traffic: "heavy",
+                repairCost: "23000",
+                installDate: new Date("December 1. 2009"),
                 cells: [
                     {
                         range: "small"
@@ -38,6 +81,7 @@ var app = angular.module("networkManager", ["ngRoute"])
                 ]
             }
         ];
+        $scope.rowLimit = 3;
         $scope.nodes = nodes;
         $scope.networkName = "Network Overview";
         $scope.deleteNode = function (node) {
