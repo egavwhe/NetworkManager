8b49843a4d4a2c0d87d68cf262317230633a4647
egavwhe <gavin.whelan@ericsson.com>
2016-09-25 22:38:55 +0100

    Loading JSON data from file.

--- networkManagerApp/app.js
+++ networkManagerApp/app.js
@@ -9,79 +9,16 @@ var app = angular.module("networkManager", ["ngRoute"])
                 templateUrl: "templates/network.html",
                 controller: "networkController"
             })
+            .otherwise({
+                redirestTo: "/home"
+            })
     })
     .controller("homeController", function ($scope) {
         $scope.message = "Welcome!";
+
     })
     .controller("networkController", function ($scope) {
-        var nodes = [
-            {
-                id: 1,
-                type: "rbs",
-                location: "Galway",
-                traffic: "heavy",
-                repairCost: "12000",
-                installDate: new Date("January 17. 2015"),
-                cells: [
-                    {
-                        range: "medium"
-                    }
-                ]
-            },
-            {
-                id: 2,
-                type: "Lte",
-                location: "Dublin",
-                traffic: "light",
-                repairCost: "23000",
-                installDate: new Date("November 17. 2012"),
-                cells: [
-                    {
-                        range: "small"
-                    }
-                ]
-            },
-            {
-                id: 3,
-                type: "Lte",
-                location: "Cork",
-                traffic: "medium",
-                repairCost: "5000",
-                installDate: new Date("March 3. 2010"),
-                cells: [
-                    {
-                        range: "small"
-                    }
-                ]
-            },
-            {
-                id: 4,
-                type: "eNodeB",
-                location: "Belfast",
-                traffic: "light",
-                repairCost: "40000",
-                installDate: new Date("April 10. 2016"),
-                cells: [
-                    {
-                        range: "small"
-                    }
-                ]
-            },
-            {
-                id: 5,
-                type: "nodeB",
-                location: "Donegal",
-                traffic: "heavy",
-                repairCost: "23000",
-                installDate: new Date("December 1. 2009"),
-                cells: [
-                    {
-                        range: "small"
-                    }
-                ]
-            }
-        ];
-        $scope.rowLimit = 3;
+        $scope.rowLimit = 8;
         $scope.nodes = nodes;
         $scope.networkName = "Network Overview";
         $scope.deleteNode = function (node) {
